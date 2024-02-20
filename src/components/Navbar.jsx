90245f4e7a5d25e3591e1894babb95927ab5c89a
Jaydeep Singh <jaydeepsingh1919@gmail.com>
2024-02-20 19:36:37 +0530

    final push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
         {link: "Feature", path: "feature"},
         {link: "About", path: "about"},
         {link: "Pricy", path: "pricing"},
+        {link: "Contact", path: "pricing"},
         
     ]
     return (
